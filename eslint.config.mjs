0c54413b2485583d0c2a0426ed612667e167f249
Rakib Hassan <mailrakibhassan@gmail.com>
2024-05-21 21:50:02 +0600

    eslint and prettier setup completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ['dist/**/*'],
+  },
+  {
+    files: ['src/**/*'],
+    rules: {
+      'no-console': 'warn',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  eslintConfigPrettier,
 ];
